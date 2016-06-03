51013a1a79a7856ed2b31f8d2e6d3adb27761d70
Charles Humphries <humphrcd@dukes.jmu.edu>
2016-06-03 14:36:13 -0400

    Added Favicon

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const nodemailer = require("nodemailer");
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
+const favicon = require('serve-favicon');
 
 var port = process.env.PORT || 8080;
 const app = express();
@@ -10,6 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use("/css", express.static(__dirname + '/css'));
 app.use("/js", express.static(__dirname + '/js'));
 app.use("/images", express.static(__dirname + '/images'));
+app.use(favicon(__dirname + '/images/favicon.ico'));
 app.set('view engine', 'ejs');
 
 /*
